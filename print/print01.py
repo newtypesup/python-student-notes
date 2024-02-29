cb07927210245fb95d3e7f13896fd25f2c7fa77d
BitSup92 <shssyup2@gmail.com>
2024-02-29 12:43:16 +0900

    commit

--- print/print01.py
+++ print/print01.py
@@ -14,18 +14,4 @@ print(a+b)
 print('가'+'3')
 print('Hello~ Python~'*20)
 
-print('asdf')
-print(5)
-print(-5)
-print(3.00)
-print(3.1425)
-print(5+3+2+1)
-print(5+2*4)
-a=5
-b=3+2*4
-a+=2
-print(a)
-print(b)
-print(a+b)
-print('가'+'3')
-print('Hello~ Python~'*20)
+
